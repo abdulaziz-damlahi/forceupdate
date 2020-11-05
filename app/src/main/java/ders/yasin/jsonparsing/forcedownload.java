0c9271286eb8b7cf4c6c87a00f25ae8f849322fd
abdulaziz-damlahi <abood_d@live.com>
2020-11-05 17:09:32 +0300

    download animation added

--- app/src/main/java/ders/yasin/jsonparsing/forcedownload.java
+++ app/src/main/java/ders/yasin/jsonparsing/forcedownload.java
@@ -11,6 +11,7 @@ import android.widget.Button;
 
 public class forcedownload extends Activity {
     Button update;
+
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
 
